db1aa3765cd305fdd3947b277672c6431be951db
wei hua <as.weihua@aisino.com>
2021-08-05 17:39:08 +0800

    序列化

--- use_pickle.py
+++ use_pickle.py
@@ -3,18 +3,16 @@
 
 __author__ = 'HUA'
 
-
 import pickle
 
-d = dict(name='Bob' , age = 20, score = 88)
+d = dict(name='Bob', age=20, score=88)
 data = pickle.dumps(d)
 print(data)
-with open('dump.txt', 'wb') as f :
+with open('dump.txt', 'wb') as f:
     f.write(data)
 
-with open('dump.txt', 'rb') as f :
+with open('dump.txt', 'rb') as f:
     print(pickle.load(f))
-
 
 reborn = pickle.loads(data)
 print(reborn)
