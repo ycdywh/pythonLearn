e5dbd5d62fbe9081a969a500c778f80c25f326b6
wei hua <as.weihua@aisino.com>
2021-09-15 16:41:06 +0800

    contextlib

---
+++ use_contextlib.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+
+from contextlib import contextmanager
+
+
+class Query(object):
+    def __init__(self, name):
+        self.name = name
+
+    def query(self):
+        print('Query info about %s...' % self.name)
+
+
+@contextmanager
+def create_query(name):
+    print('Begin')
+    q = Query(name)
+    yield q
+    print('End')
+
+
+with create_query('Bob') as q:
+    q.query()
+
+
+@contextmanager
+def tag(name):
+    print('<%s>' % name)
+    yield
+    print('</%s>' % name)
+
+
+with tag('h1'):
+    print('hello')
+    print('world')
+
+from contextlib import closing
+from urllib.request import urlopen
+
+with closing(urlopen('https://www.python.org')) as page:
+    for line in page:
+        print(line)
